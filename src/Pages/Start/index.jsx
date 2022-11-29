485eb5eedd90547b0dcd90a87d2c88823d3100b8
Daniel Taccola <danieltaccola@yahoo.com.br>
2022-11-28 21:50:59 -0300

    Estilização da ágina start

--- src/Pages/Start/index.jsx
+++ src/Pages/Start/index.jsx
@@ -1,19 +1,41 @@
 import React from "react";
-import { View, ScrollView, Text, Image } from "react-native";
+import { View, ScrollView, Text, Image, StyleSheet } from "react-native";
 
 export default function Start() {
     return(
-        <View>
+        <View style={styles.container}>
             <ScrollView showVerticalScrollIndicator={false}>
-                <View>
+                <View style={{ alignItems: "center" }}>
                     <Image
-                        source={requestAnimationFrame("../../assets/icons/logo3.png")}
+                        source={require("../../assets/icons/logo3.png")}
+                        style={styles.logo}
                     />
-                    <Text>
-                        Vamos transformar sua vida {"/n"} em jogo , buscando sempre {"/n"} o melhor nível
+                    <Text style={styles.description}>
+                        Vamos transformar sua vida {"\n"} em jogo , buscando sempre {"\n"} o melhor nível
                     </Text>
                 </View>
             </ScrollView>
         </View>    
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex:1,
+        backgroundColor: "rgba(21, 21, 21, 0.98)",
+    },
+
+    logo: {
+        width: 300,
+        height: 60,
+        marginTop: 60,
+        marginBottom: 20,  
+    },
+
+    description: {
+        color: "#FFF",
+        fontSize: 20,
+        textAlign: "center",
+        marginVertical: 60,
+    },
+});
