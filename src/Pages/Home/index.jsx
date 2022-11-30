395ac2b69c78994359e2924fbe5801ea54fca687
Daniel Taccola <danieltaccola@yahoo.com.br>
2022-11-30 09:32:03 -0300

    barra de status

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -3,6 +3,7 @@ import { useNavigation } from "@react-navigation/native";
 import { View, ScrollView, Text, StyleSheet } from "react-native";
 
 import LifeStatus from "../../Components/Common/LifeStatus"
+import StatusBar from "../../Components/Home/StatusBar";
 
 export default function Home() {
     const navigation = useNavigation();
@@ -16,7 +17,9 @@ export default function Home() {
             <ScrollView>
                 <View style={{ alignItems: "center" }}>
                     <Text style={styles.dailyChecks}> ❤️ 20 dias - ✔️ 80 checks</Text>
+
                     <LifeStatus />
+                    <StatusBar />
                 </View>
                 <Text
                     style={styles.explanationText}
