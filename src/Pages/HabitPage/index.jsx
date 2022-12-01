c0296d75c18e43b76463849841c62878d771e9b1
Daniel Taccola <danieltaccola@yahoo.com.br>
2022-11-30 21:19:36 -0300

    dropdown selecionar frequencia

--- src/Pages/HabitPage/index.jsx
+++ src/Pages/HabitPage/index.jsx
@@ -11,10 +11,12 @@ import {
 
 import { useNavigation } from "@react-navigation/native";
 import SelectHabit from "../../Components/HabitPage/SelectHabit";
+import SelectFrequency from "../../Components/HabitPage/SelectFrequency";
 
 export default function HabitPage({ route }) {
     const navigation = useNavigation();
     const [habitInput, setHabitInput] = useState();
+    const [frequencyInput, setFrequencyInput] = useState();
     const { create, habit } = route.params;
 
     return (
@@ -36,8 +38,15 @@ export default function HabitPage({ route }) {
                         <View style={styles.inputContainer}>
                             <Text style={styles.area}>{habit?.habitArea}</Text>
                         </View>
+
                         <Text style={styles.inputText}>Hábito</Text>
                         <SelectHabit habit={habit} habitInput={setHabitInput} />
+
+                        <Text style={styles.inputText}>Frequência</Text>
+                        <SelectFrequency
+                            habitFrequency={habit?.habitFrequency}
+                            frequencyInput={setFrequencyInput}
+                        />
                     </View>
                 </View>
             </ScrollView>
